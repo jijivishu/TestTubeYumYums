585045641fc611f1d43e75ea61eb8c4bed4a0dd3
jijivishu <70134939+jijivishu@users.noreply.github.com>
2024-01-08 15:52:58 +0530

    Updated database name

--- Capstone/settings.py
+++ Capstone/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'Capstone.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'TestTubeYumYumsNew',
+        'NAME': 'TestTubeYumYumsDB',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': 'localhost',
